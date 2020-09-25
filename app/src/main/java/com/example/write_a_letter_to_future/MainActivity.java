9f358f30eccdc1087e745bbfe6844bd900f0a393
Bhavana Jami <bhavanajami111@gmail.com>
2020-09-25 16:17:12 +0530

    successful google sign in

--- app/src/main/java/com/example/write_a_letter_to_future/MainActivity.java
+++ app/src/main/java/com/example/write_a_letter_to_future/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.write_a_letter_to_future;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.accounts.AccountManagerFuture;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
@@ -19,6 +20,8 @@ import android.widget.Toast;
 import com.airbnb.lottie.LottieAnimationView;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInApi;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
@@ -32,44 +35,80 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
 
 public class MainActivity extends AppCompatActivity {
-    private static final int RC_SIGN_IN = 2;
-    private static final String TAG = "Error";
-    private static int SPLASH_SCREEN=5000;//5000 here decides for howlong the splash screen need to stand
-    Animation topAnime;
-    Animation bottomAnime;
-    TextView line,tag;
-    LottieAnimationView mailbox;
-    private Object mGoogleSignInClient;
-    SignInButton sign_in;
-    private FirebaseAuth mAuth;
+    private static final int RC_SIGN_IN = 0;
+    SignInButton signin;
+    private GoogleSignInClient mGoogleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //For full scree,splash screen
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
         //Animations
-        topAnime=AnimationUtils.loadAnimation(this,R.anim.top_anime);
-        bottomAnime=AnimationUtils.loadAnimation(this,R.anim.bottom_anime);
-        line=findViewById(R.id.app_name);
-        tag=findViewById(R.id.tag_name);
-        mailbox=findViewById(R.id.lottieAnimationView);
-        sign_in= (SignInButton) findViewById(R.id.signin_btn);
-        //SETTING the animations to the views
-       /* line.setAnimation(bottomAnime);
-        tag.setAnimation(bottomAnime);
-        mAuth=FirebaseAuth.getInstance();*/
-        //for moving to the dashboard screent after loading the splash screen
-       /* new Handler().postDelayed(new Runnable() {
+       /* topAnime = AnimationUtils.loadAnimation(this, R.anim.top_anime);
+        bottomAnime = AnimationUtils.loadAnimation(this, R.anim.bottom_anime);
+        line = findViewById(R.id.app_name);
+        tag = findViewById(R.id.tag_name);
+        mailbox = findViewById(R.id.lottieAnimationView);*/
+
+
+        //****Here starts our Google sign in button code
+        signin = (SignInButton) findViewById(R.id.signin_btn);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        signin.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void run() {
-                Intent intent=new Intent(MainActivity.this,dashboard.class);
-                startActivity(intent);
-                finish();//must remember line of the code,coz it allows u to close the app directly without moving to the splash screen again
+            public void onClick(View view) {
+                switch (view.getId()) {
+                    case R.id.signin_btn:
+                        signIn();
+                        break;
+                    // ...
+                }
             }
-        },SPLASH_SCREEN);*/
+        });
 
+    }
 
+    private void signIn(){
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+            if (requestCode == RC_SIGN_IN) {
+                Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+                handleSignInResult(task);
+            }
+    }
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            // Signed in successfully, show authenticated UI.
+            Intent intent=new Intent(MainActivity.this,dashboard.class);
+            startActivity(intent);
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Toast.makeText(this, "Signin failed", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override//This is for--if the user is logged into the app then direct  him to dashboard instead  of making sign in againa
+    protected void onStart() {
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        if(account!=null){
+            startActivity(new Intent(MainActivity.this,dashboard.class));
+        }
+        super.onStart();
     }
 }
--- app/src/main/java/com/example/write_a_letter_to_future/dashboard.java
+++ app/src/main/java/com/example/write_a_letter_to_future/dashboard.java
@@ -1,41 +1,95 @@
 package com.example.write_a_letter_to_future;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 
+import org.w3c.dom.Text;
+
+import static com.google.android.gms.auth.api.signin.GoogleSignInOptions.DEFAULT_SIGN_IN;
+
 public class dashboard extends AppCompatActivity {
-    Button logout;
-    TextView name,mail;
+    TextView user_name,user_mail;
+    ImageView user_profile;
+    Button user_logout;
+    GoogleSignInClient GoogleSignInClient;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
-        logout=(Button)findViewById(R.id.logout_btn);
-        name=(TextView)findViewById(R.id.name);
-        mail=(TextView)findViewById(R.id.mail);
-
-        /*GoogleSignInAccount signInAccount= GoogleSignIn.getLastSignedInAccount(this);
-        if(signInAccount!=null){
-            name.setText(signInAccount.getDisplayName());
-            mail.setText(signInAccount.getEmail());
-        }
-
-        logout.setOnClickListener(new View.OnClickListener() {
+        user_name = (TextView) findViewById(R.id.name);
+        user_mail = (TextView) findViewById(R.id.email);
+        user_profile = (ImageView) findViewById(R.id.profile);
+        user_logout = (Button) findViewById(R.id.logout_btn);
+        //for signout
+       /* user_logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                FirebaseAuth.getInstance().signOut();
-                Intent intent=new Intent(getApplicationContext(),MainActivity.class);
-                startActivity(intent);
+                switch (view.getId()) {
+                    // ...
+                    case R.id.user_logout:
+                        signOut();
+                        break;
+                    // ...
+                }
             }
         });*/
+        GoogleSignInOptions gso=new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        GoogleSignInClient=GoogleSignIn.getClient(this,gso);
+
+        //***To get the user information
+
+        GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(dashboard.this);
+
+        if (acct != null) {
+            String personName = acct.getDisplayName();
+            String personEmail = acct.getEmail();
+            Uri personPhoto = acct.getPhotoUrl();
+            //Setting the above variables to our textview fields
+            user_name.setText(personName);
+            user_mail.setText(personEmail);
+            Glide.with(this).load(new String().valueOf(personPhoto)).into(user_profile);//for getting the image..
+        }
+        user_logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signOut();
+            }
+        });
+
+
+    }
+    private void signOut(){
+        GoogleSignInClient.signOut()
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Toast.makeText(dashboard.this, "Successfully signedout", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(dashboard.this,MainActivity.class));
+                        finish();
+                    }
+                });
     }
 }
